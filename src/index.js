9bdf58446c9bccdf577d94a7fa5aeab09890c671
younesbahou123 <185143547+younesbahou123@users.noreply.github.com>
2024-11-26 23:57:33 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import "./styles.css";
+const mainEl = document.querySelector("main");
+mainEl.style.backgroundColor = "var(--main-bg)";
+mainEl.innerHTML = "<h1>DOM Manipulation</h1>";
+mainEl.classList.add("flex-ctr");
